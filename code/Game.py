2373382748225c6a4bcb9de492b067bc0b8a520b
Rafaelle <rafaellelorenacosta@gmail.com>
2026-06-11 20:23:25 -0300

    Creating Parallax

--- code/Game.py
+++ code/Game.py
@@ -4,9 +4,9 @@ import sys
 
 import pygame
 
+from code.Level import Level
 from code.Menu import Menu
-from code.Const import WIN_WIDTH, WIN_HEIGHT
-
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 
 class Game:
     def __init__(self):
@@ -16,5 +16,13 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
